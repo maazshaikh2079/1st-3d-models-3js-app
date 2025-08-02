e5d990115141cc2f69b9fd2cd1d664f837f06db8
maaz <maazshaikh2079@gmail.com>
2025-08-02 19:35:55 +0530

    4th commit

---
+++ js/futuristic_spaceship2.js
@@ -0,0 +1,73 @@
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+//Create a Three.JS Scene
+const scene = new THREE.Scene();
+//Create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//Keep the 3D object on a global variable so we can access it later
+let object;
+
+//OrbitControls allow the camera to move around the scene
+let controls;
+
+//Set which object to render
+let objToRender = 'futuristic_spaceship';
+
+//Instantiate a loader for the .gltf file
+const loader = new GLTFLoader();
+
+//Load the futuristic_spaceship model
+loader.load(
+  `./models/${objToRender}/scene.gltf`,
+  function (gltf) {
+    object = gltf.scene;
+            // object.scale.set(200, 20, 100);
+    // object.scale.set(150, 20, 350);ff
+    // object.position.set(0, 0, 0);
+    scene.add(object);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    console.error(error);
+  }
+);
+
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+//Set camera distance for futuristic_spaceship
+camera.position.z = 13;
+
+//Add lights
+const topLight = new THREE.DirectionalLight(0xffffff, 2);
+topLight.position.set(500, 500, 500);
+// topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+//Add OrbitControls for futuristic_spaceship
+controls = new OrbitControls(camera, renderer.domElement);
+
+//Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+//Handle window resize
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
